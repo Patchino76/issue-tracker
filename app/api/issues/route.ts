4efc661a0a906535a490a2f97a889eaf8ea5aec8
Svetoslav Lyubenov <patchino76@gmail.com>
2024-08-28 16:59:57 +0300

    set up redix ui

---
+++ app/api/issues/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import { Prisma } from "@prisma/client";
+import prisma from "@/prisma/client";
+
+const schemaSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const validation = schemaSchema.safeParse(body);
+
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors[0], { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: {
+      title: validation.data.title,
+      description: validation.data.description,
+    },
+  });
+
+  if(!newIssue) {
+    return NextResponse.json("Something went wrong", { status: 500 });
+  }
+  return NextResponse.json(newIssue, { status: 201 });
+}
