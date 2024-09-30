64827c4f0a80ce627bdc37d779af371f5fb4a41d
Svetoslav Lyubenov <patchino76@gmail.com>
2024-09-30 09:46:46 +0300

    Fetch data with react query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,20 +1,27 @@
-'use client';
+"use client";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Skeleton from "@/app/components/Skeleton";
 
 const AssigneeSelect = async () => {
-  const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>(
-        "http://localhost:3000/api/users"
-      );
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () =>
+      axios.get("http://localhost:3000/api/users").then((res) => res.data),
+    staleTime: 1000 * 60, // 1 minute
+    retry: 3,
+  }); 
+
+  if(isLoading) return <Skeleton />
+  if (error) return null;
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
